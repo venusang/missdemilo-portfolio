159e93cc02748625f29121e467f32c51bb8981a1
Venus Ang <venusang24@gmail.com>
2020-02-11 11:33:03 -0800

    wip created API and added redirect for demilodesigns.com

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,4 +1,12 @@
 import Route from '@ember/routing/route';
 
 export default class IndexRoute extends Route {
+    async model() {
+        const response = await fetch('/api/projects.json');
+        const projects = await response.json();
+
+        const response2 = await fetch('api/intro.json');
+        const intro = await response2.json();
+        return { intro, projects };
+    }
 }
--- app/routes/projects.js
+++ app/routes/projects.js
@@ -1,4 +1,5 @@
 import Route from '@ember/routing/route';
 
 export default class ProjectsRoute extends Route {
+
 }
