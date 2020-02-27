df2ce24edd0539e61437044a68d927a57bf5e36c
Venus Ang <venusang24@gmail.com>
2020-02-27 14:06:56 -0800

    removed dupe key

--- mirage/scenarios/default.js
+++ mirage/scenarios/default.js
@@ -1,41 +1,42 @@
-
 let jsonDB = {
   projects: [
     {
-      "type": "projects",
-      "id": "gb-landscape-design",
-      "title": "GB Landscape Design",
-      "client": "GB Landscape Design",
-      "website": ["http://gb-landscape-design.netlify.com/"],
-      "github": ["https://github.com/venusang/gb-landscapes"],
-      "services": [
-        "Web Development",
-        "Web Design"
-      ],
-      "github": ['https://github.com/venusang/gb-landscapes'],
-      "thumbnail": "gb-landscape-logo.png",
-      "screenshots": [
-        "gb-landscape/gb-landscape-01.png"
-      ],
-      "previous": "cantinas-ranch",
-      "next": "corespring"
+      type: "projects",
+      id: "gb-landscape-design",
+      title: "GB Landscape Design",
+      client: "GB Landscape Design",
+      website: ["http://gb-landscape-design.netlify.com/"],
+      github: ["https://github.com/venusang/gb-landscapes"],
+      services: ["Web Development", "Web Design"],
+      thumbnail: "gb-landscape-logo.png",
+      screenshots: ["gb-landscape/gb-landscape-01.png"],
+      previous: "cantinas-ranch",
+      next: "corespring"
     },
     {
-      "type": "projects",
-      "id": "corespring",
-      "title": "Corespring",
-      "client": "Corespring",
-      "website": ['https://missdemilo-des-12.netlify.com/', 'https://missdemilo-ca-2064.netlify.com/', 'https://missdemilo-ca-1666.netlify.com/'],
-      "github": ['https://github.com/venusang/des-12', 'https://github.com/venusang/ca-2064', 'https://github.com/venusang/ca-1666'],
-      "services": [
+      type: "projects",
+      id: "corespring",
+      title: "Corespring",
+      client: "Corespring",
+      website: [
+        "https://missdemilo-des-12.netlify.com/",
+        "https://missdemilo-ca-2064.netlify.com/",
+        "https://missdemilo-ca-1666.netlify.com/"
+      ],
+      github: [
+        "https://github.com/venusang/des-12",
+        "https://github.com/venusang/ca-2064",
+        "https://github.com/venusang/ca-1666"
+      ],
+      services: [
         "Rapid Prototyping",
         "UI/UX Design",
         "Web Application Design",
         "Web Design",
         "Angular"
       ],
-      "thumbnail": "corespring-logo.png",
-      "screenshots": [
+      thumbnail: "corespring-logo.png",
+      screenshots: [
         "corespring/des-12.png",
         "corespring/ca-2064-01.png",
         "corespring/ca-2064-02.png",
@@ -43,17 +44,16 @@ let jsonDB = {
         "corespring/Corespring-Homepage-Before1.png",
         "corespring/Corespring-Homepage-After1.png"
       ],
-      "previous": "gb-landscape-design",
-      "next": "leaders-cosmetics"
+      previous: "gb-landscape-design",
+      next: "leaders-cosmetics"
     },
     {
-
-      "type": "projects",
-      "id": "leaders-cosmetics",
-      "title": "Leaders Cosmetics USA",
-      "client": "Leaders Cosmetics USA",
-      "github": ['https://github.com/venusang/leaders'],
-      "services": [
+      type: "projects",
+      id: "leaders-cosmetics",
+      title: "Leaders Cosmetics USA",
+      client: "Leaders Cosmetics USA",
+      github: ["https://github.com/venusang/leaders"],
+      services: [
         "E-Commerce",
         "Logo Design",
         "Marketing Landing Pages",
@@ -63,31 +63,31 @@ let jsonDB = {
         "Web Design",
         "Web Development"
       ],
-      "thumbnail": "leaders-logo.png",
-      "screenshots": [
+      thumbnail: "leaders-logo.png",
+      screenshots: [
         "leaders/popover-before-957x667.png",
         "leaders/popover-after-957x667.png",
         "leaders/leaders-homepage-before.png",
         "leaders/leaders-homepage-after.png"
       ],
-      "previous": "corespring",
-      "next": "trove"
+      previous: "corespring",
+      next: "trove"
     },
     {
-      "type": "projects",
-      "id": "trove",
-      "title": "Trove",
-      "client": "Trove",
-      "services": [
+      type: "projects",
+      id: "trove",
+      title: "Trove",
+      client: "Trove",
+      services: [
         "Web Design",
         "Web Development",
         "Responsive Web",
         "UI/UX Design"
       ],
-      "website": ['https://missdemilo-trove.netlify.com/#/dashboard'],
-      "github": ["https://github.com/venusang/trove-demo"],
-      "thumbnail": "trove-logo.png",
-      "screenshots": [
+      website: ["https://missdemilo-trove.netlify.com/#/dashboard"],
+      github: ["https://github.com/venusang/trove-demo"],
+      thumbnail: "trove-logo.png",
+      screenshots: [
         "trove/dashboard.png",
         "trove/trove-02.png",
         "trove/trove-03.png",
@@ -98,20 +98,20 @@ let jsonDB = {
         "trove/trove-08.png",
         "trove/trove-09.png"
       ],
-      "services": ["Web Development, Ember.js (2013), Rapid Prototyping, High Charts"],
-      "previous": "leaders-cosmetics",
-      "next": "d3"
+      services: [
+        "Web Development, Ember.js (2013), Rapid Prototyping, High Charts"
+      ],
+      previous: "leaders-cosmetics",
+      next: "d3"
     },
     {
-      "type": "projects",
-      "id": "d3",
-      "title": "D3",
-      "client": "D3",
-      "services": [
-        "UI/UX Design"
-      ],
-      "thumbnail": "d3-logo.png",
-      "screenshots": [
+      type: "projects",
+      id: "d3",
+      title: "D3",
+      client: "D3",
+      services: ["UI/UX Design"],
+      thumbnail: "d3-logo.png",
+      screenshots: [
         "d3/D3-general-information-before.png",
         "d3/D3-general-information-after.png",
         "d3/Screenshot-2015-06-23-18.45.18-810x667.png",
@@ -119,33 +119,27 @@ let jsonDB = {
         "d3/Screenshot-2015-06-23-18.46.59.png",
         "d3/Screenshot-2015-06-23-18.47.24-983x667.png"
       ],
-      "previous": "trove",
-      "next": "gtd-multisport"
+      previous: "trove",
+      next: "gtd-multisport"
     },
     {
-      "type": "projects",
-      "id": "gtd-multisport",
-      "title": "GTD Multisport",
-      "client": "GTD Multisport",
-      "website": ["http://gtdmultisport.com"],
-      "services": [
-        "Web Development",
-        "Design",
-        "WordPress"
-      ],
-      "thumbnail": "gtd-logo.png",
-      "screenshots": [
-        "gtd/gtd-screenshot.jpg"
-      ],
-      "previous": "d3",
-      "next": "caa"
+      type: "projects",
+      id: "gtd-multisport",
+      title: "GTD Multisport",
+      client: "GTD Multisport",
+      website: ["http://gtdmultisport.com"],
+      services: ["Web Development", "Design", "WordPress"],
+      thumbnail: "gtd-logo.png",
+      screenshots: ["gtd/gtd-screenshot.jpg"],
+      previous: "d3",
+      next: "caa"
     },
     {
-      "type": "projects",
-      "id": "caa",
-      "title": "CAA",
-      "client": "Creative Artists Agency (CAA)",
-      "services": [
+      type: "projects",
+      id: "caa",
+      title: "CAA",
+      client: "Creative Artists Agency (CAA)",
+      services: [
         "WordPress",
         ".NET",
         "Marketing Landing Pages",
@@ -154,8 +148,8 @@ let jsonDB = {
         "Web Design",
         "Web Development"
       ],
-      "thumbnail": "caa-logo.png",
-      "screenshots": [
+      thumbnail: "caa-logo.png",
+      screenshots: [
         "caa/CAASpeakers-mock-12-13-2011-homepage-grey.jpg",
         "caa/CAASpeakers-mock-v1a-homepage.jpg",
         "caa/CAASpeakers-wireframe.png",
@@ -163,156 +157,131 @@ let jsonDB = {
         "caa/tourRouter-2.png",
         "caa/tourRouter-3.png"
       ],
-      "previous": "gtd-multisport",
-      "next": "anypresence"
+      previous: "gtd-multisport",
+      next: "anypresence"
     },
     {
-      "type": "projects",
-      "id": "anypresence",
-      "title": "AnyPresence",
-      "client": "AnyPresence",
-      "services": [
+      type: "projects",
+      id: "anypresence",
+      title: "AnyPresence",
+      client: "AnyPresence",
+      services: [
         "Web Development",
         "Backbone.js",
         "Ruby on Rails",
         "UI/UX Design",
         "Mobile Graphic Design"
       ],
-      "github": ['https://github.com/venusang/mc'],
-      "thumbnail": "anypresence-logo.png",
-      "screenshots": [
+      github: ["https://github.com/venusang/mc"],
+      thumbnail: "anypresence-logo.png",
+      screenshots: [
         "anypresence/ap-mc-1000x614.png",
         "anypresence/ap-leon-1000x614.png"
       ],
-      "previous": "caa",
-      "next": "ventata"
+      previous: "caa",
+      next: "ventata"
     },
     {
-      "type": "projects",
-      "id": "ventata",
-      "title": "Ventata",
-      "client": "Ventata",
-      "services": [
+      type: "projects",
+      id: "ventata",
+      title: "Ventata",
+      client: "Ventata",
+      services: [
         "Web Development",
         "UI/UX Design",
         "Graphic Design",
         "Logo Design"
       ],
-      "thumbnail": "ventata-logo.png",
-      "screenshots": [
-        "ventata/ventata-01.png"
-      ],
-      "previous": "anypresence",
-      "next": "rosenlaw"
+      thumbnail: "ventata-logo.png",
+      screenshots: ["ventata/ventata-01.png"],
+      previous: "anypresence",
+      next: "rosenlaw"
     },
     {
-      "type": "projects",
-      "id": "rosenlaw",
-      "title": "Rosen Law Firm",
-      "client": "Rosen Law Firm",
-      "website": ["http://rosenlawfirm.info/"],
-      "services": [
-        "Web Development",
-        "UI/UX Design",
-        "WordPress"
-      ],
-      "thumbnail": "rosenlawfirm-logo.png",
-      "screenshots": [
-        "rosenlaw/rosen-law-firm.png"
-      ],
-      "previous": "ventata",
-      "next": "fcci"
+      type: "projects",
+      id: "rosenlaw",
+      title: "Rosen Law Firm",
+      client: "Rosen Law Firm",
+      website: ["http://rosenlawfirm.info/"],
+      services: ["Web Development", "UI/UX Design", "WordPress"],
+      thumbnail: "rosenlawfirm-logo.png",
+      screenshots: ["rosenlaw/rosen-law-firm.png"],
+      previous: "ventata",
+      next: "fcci"
     },
     {
-      "type": "projects",
-      "id": "fcci",
-      "title": "First Capitol Consulting Inc.",
-      "date": "2013",
-      "client": "First Capitol Consulting Inc.",
-      "services": [
+      type: "projects",
+      id: "fcci",
+      title: "First Capitol Consulting Inc.",
+      date: "2013",
+      client: "First Capitol Consulting Inc.",
+      services: [
         "Logo Design",
         "UI/UX Design",
         "Web Design",
         "Web Development",
         "WordPress Development"
       ],
-      "thumbnail": "fcci-logo.png",
-      "screenshots": [
-        "fcci/fcci-before.png",
-        "fcci/fcci-after.png"
-      ],
-      "previous": "rosenlaw",
-      "next": "smart-living-company"
+      thumbnail: "fcci-logo.png",
+      screenshots: ["fcci/fcci-before.png", "fcci/fcci-after.png"],
+      previous: "rosenlaw",
+      next: "smart-living-company"
     },
     {
-      "type": "projects",
-      "id": "smart-living-company",
-      "title": "Smart Living Company",
-      "client": "Smart Living Company",
-      "services": [
+      type: "projects",
+      id: "smart-living-company",
+      title: "Smart Living Company",
+      client: "Smart Living Company",
+      services: [
         "E-Commerce",
         "Marketing Landing Pages",
         "UI/UX Design",
         "Web Design"
       ],
-      "thumbnail": "slc-logo.png",
-      "screenshots": [
+      thumbnail: "slc-logo.png",
+      screenshots: [
         "slc/slc-homepage-before-after.png",
         "slc/slc-login-before-after2-1000x479.png",
         "slc/slc-preferences-before1.png",
         "slc/slc-preferences-after1.jpg"
       ],
-      "previous": "fcci",
-      "next": "easton-robertson"
+      previous: "fcci",
+      next: "easton-robertson"
     },
     {
-      "type": "projects",
-      "id": "easton-robertson",
-      "title": "Easton Robertson Insurance",
-      "client": "Easton Robertson Insurance",
-      "services": [
-        "Web Development",
-        "UI/UX Design",
-        "Web Design",
-        "WordPress"
-      ],
-      "thumbnail": "easton-robertson-logo.png",
-      "screenshots": [
-        "eastonrobertson/easton-robertson.png"
-      ],
-      "previous": "smart-living-company",
-      "next": "myspace"
+      type: "projects",
+      id: "easton-robertson",
+      title: "Easton Robertson Insurance",
+      client: "Easton Robertson Insurance",
+      services: ["Web Development", "UI/UX Design", "Web Design", "WordPress"],
+      thumbnail: "easton-robertson-logo.png",
+      screenshots: ["eastonrobertson/easton-robertson.png"],
+      previous: "smart-living-company",
+      next: "myspace"
     },
     {
-      "type": "projects",
-      "id": "myspace",
-      "title": "MySpace",
-      "client": "MySpace",
-      "services": [
-        "Web Development",
-        "Web Design"
-      ],
-      "thumbnail": "myspace-logo.png",
-      "screenshots": [
+      type: "projects",
+      id: "myspace",
+      title: "MySpace",
+      client: "MySpace",
+      services: ["Web Development", "Web Design"],
+      thumbnail: "myspace-logo.png",
+      screenshots: [
         "myspace/BSG-Sept2006.jpg",
         "myspace/HerbalCupid-hub1.jpg",
         "myspace/Secret-Mashup1.png"
       ],
-      "previous": "easton-robertson",
-      "next": "experian"
+      previous: "easton-robertson",
+      next: "experian"
     },
     {
-      "type": "projects",
-      "id": "experian",
-      "title": "Experian",
-      "client": "Experian",
-      "services": [
-        "Web Development",
-        "Web Design",
-        "Email Marketing"
-      ],
-      "thumbnail": "experian-logo.png",
-      "screenshots": [
+      type: "projects",
+      id: "experian",
+      title: "Experian",
+      client: "Experian",
+      services: ["Web Development", "Web Design", "Email Marketing"],
+      thumbnail: "experian-logo.png",
+      screenshots: [
         "experian/b2b_final.jpg",
         "experian/BizInsight2008.jpg",
         "experian/BOLv2_final1.jpg",
@@ -320,67 +289,56 @@ let jsonDB = {
         "experian/footballMock_v21.jpg",
         "experian/Holiday2007Email-v11.jpg"
       ],
-      "previous": "myspace",
-      "next": "chevy"
+      previous: "myspace",
+      next: "chevy"
     },
     {
-      "type": "projects",
-      "id": "chevy",
-      "title": "Chevy",
-      "client": "Chevy",
-      "services": [
-        "Web Design",
-        "Print Design"
-      ],
-      "thumbnail": "chevy-logo.png",
-      "screenshots": [
-        "chevy/SkiChevy_layout_final.jpg"
-      ],
-      "previous": "experian",
-      "next": "fox"
+      type: "projects",
+      id: "chevy",
+      title: "Chevy",
+      client: "Chevy",
+      services: ["Web Design", "Print Design"],
+      thumbnail: "chevy-logo.png",
+      screenshots: ["chevy/SkiChevy_layout_final.jpg"],
+      previous: "experian",
+      next: "fox"
     },
     {
-      "type": "projects",
-      "id": "fox",
-      "title": "Fox",
-      "client": "Fox",
-      "services": [
-        "Flash Development"
-      ],
-      "thumbnail": "fox-logo.png",
-      "screenshots": [
+      type: "projects",
+      id: "fox",
+      title: "Fox",
+      client: "Fox",
+      services: ["Flash Development"],
+      thumbnail: "fox-logo.png",
+      screenshots: [
         "fox/fox-01.png",
         "fox/fox-02.png",
         "fox/fox-03.png",
         "fox/fox-04.png"
       ],
-      "previous": "chevy",
-      "next": "cantinas-ranch"
+      previous: "chevy",
+      next: "cantinas-ranch"
     },
     {
-      "type": "projects",
-      "id": "cantinas-ranch",
-      "title": "Cantinas Ranch",
-      "client": "Cantinas Ranch",
-      "services": [
-        "Web Development",
-        "Web Design",
-        "WordPress Development"
-      ],
-      "github": ['https://github.com/venusang/cantinasranch'],
-      "thumbnail": "cantinas-logo.png",
-      "screenshots": [
+      type: "projects",
+      id: "cantinas-ranch",
+      title: "Cantinas Ranch",
+      client: "Cantinas Ranch",
+      services: ["Web Development", "Web Design", "WordPress Development"],
+      github: ["https://github.com/venusang/cantinasranch"],
+      thumbnail: "cantinas-logo.png",
+      screenshots: [
         "cantinas/cantinas-ranch-logo-comparison-1000x614.png",
         "cantinas/homepage-before-after.jpg",
         "cantinas/boardmembers-before-after.jpg"
       ],
-      "previous": "fox",
-      "next": "gb-landscape-design"
+      previous: "fox",
+      next: "gb-landscape-design"
     }
   ]
-}
-export default function (server) {
+};
+export default function(server) {
   jsonDB.projects.forEach(project => {
-    server.create('project', project);
+    server.create("project", project);
   });
 }
