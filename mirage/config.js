a3a80e084b5e7e7df7e060ef6eaa097d573350ba
Venus Ang <venusang24@gmail.com>
2020-02-28 14:13:04 -0800

    adding mirage fixtures

--- mirage/config.js
+++ mirage/config.js
@@ -1,5 +1,5 @@
-export default function () {
-  this.namespace = '/api';
-  this.get('/projects');     // returns all projects in the mirage database
-  this.get('/projects/:id');
+export default function() {
+  this.namespace = "/api";
+  this.get("/projects"); // returns all projects in the mirage database
+  this.get("/projects/:id");
 }
